8ccf3a9b8b98e8a6ed9348249cbbca8dc416dfc0
ContardiC <c.contardi@icloud.com>
2022-11-08 15:54:19 +0100

    array di oggetti

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputStreamReader in= new InputStreamReader(System.in);
+        BufferedReader keyboard=new BufferedReader(in);
+        Mesi mesi = new Mesi();
+        int numeroMese;
+        try{
+            System.out.println("Inserisci il numero di un mese (da 1 a 12)");
+            numeroMese=Integer.parseInt(keyboard.readLine());
+            System.out.println("Mese: "+ mesi.getMese(numeroMese)+ " ed ha "+mesi.getGiorni(numeroMese)+" giorni");
+
+        }catch(IOException e){
+
+        }
     }
 }
