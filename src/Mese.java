5b80b989c21216c969fdc25590628caffe61b761
Carlo Contardi <c.contardi@icloud.com>
2022-11-08 11:24:34 +0100

    Mesi added

---
+++ src/Mese.java
@@ -0,0 +1,14 @@
+public class Mese {
+    private String nome;
+    private int giorni;
+    public Mese(String n, int g) {
+        nome = n;
+        giorni = g;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getGiorni(){
+        return giorni;
+    }
+}
