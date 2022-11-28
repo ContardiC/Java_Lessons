76624dd250e93a2a150e4d42e9ed67a8789ee5af
ContardiC <c.contardi@icloud.com>
2022-11-28 16:09:18 +0100

    esercitazione Array di Oggetti added

--- src/spacecoding/esercitazioni/libri/BooksManager.java
+++ src/spacecoding/esercitazioni/libri/BooksManager.java
@@ -1,4 +1,59 @@
 package spacecoding.esercitazioni.libri;
 
 public class BooksManager {
+    public static Book[] arrayByAuthor(Book[] books, String authorFn, String authorLn){
+        Book[] selection=new Book[100];
+        int j=0,i=0;
+        while(books[i]!=null){
+            if(books[i].getAuthorFirstName().equals(authorFn)&&books[i].getAuthorLastName().equals(authorLn)){
+                selection[j]=books[i];
+                j++;
+            }
+            i++;
+        }
+        return selection;
+    }
+    public static Book maxPages(Book[] book){
+        int max;
+        max=book[0].getPagesNumber();
+        int i=1, index=0;
+        while(book[i]!=null){
+            if(book[i].getPagesNumber()>max){
+                max=book[i].getPagesNumber();
+                index=i;
+            }
+            i++;
+        }
+        return book[index];
+    }
+    public static boolean isExist(Book[] book,String title){
+        int i=0;
+        while(book[i]!=null){
+            if(book[i].getTitle().equals(title)){
+              return true;
+            }
+            i++;
+        }
+        return false;
+    }
+    public static int indexByTitle(Book[] book,String title){
+        int i=0;
+        while(book[i]!=null){
+            if(book[i].getTitle().equals(title)){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+    public static Book bookByTitle(Book[] book,String title){
+        int i=0;
+        while(book[i]!=null){
+            if(book[i].getTitle().equals(title)){
+                return book[i];
+            }
+            i++;
+        }
+        return null;
+    }
 }
