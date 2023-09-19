a4882b1df06212674b5ee1f4fa6b575ed0dcea29
ContardiC <c.contardi@icloud.com>
2023-09-19 11:20:33 +0200

    Socket added

--- src/spacecoding/socket/Server.java
+++ src/spacecoding/socket/Server.java
@@ -2,22 +2,18 @@ package spacecoding.socket;
 
 import java.net.*;
 import java.io.*;
-public class Server{
-    public static void main(String[] args) {
-        try{
+
+public class Server {
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
         Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true, null);
-        InputStreamReader i = new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader in = new BufferedReader(i);
-        String inputLine = in.readLine();
-        while((inputLine = in.readLine()) != null){
-             out.println(inputLine);
+        PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            out.println(inputLine);
         }
-        }catch(IOException e){
-
-        }
-        
     }
-          
 }
