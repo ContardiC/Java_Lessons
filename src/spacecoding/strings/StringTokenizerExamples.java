d8a495b7a3f48c94cc08beb6ed3a5bf0fa8a3fde
ContardiC <c.contardi@icloud.com>
2023-11-15 18:17:57 +0100

    StringTokenizer example added

---
+++ src/spacecoding/strings/StringTokenizerExamples.java
@@ -0,0 +1,21 @@
+package spacecoding.strings;
+
+import java.util.StringTokenizer;
+
+/**
+ * StringTokenizerExamples
+ */
+public class StringTokenizerExamples {
+    public static void main(String[] args) {
+        // esempio di token 
+        StringTokenizer str = new StringTokenizer("cat dog otter");
+        while(str.hasMoreTokens()){
+            System.out.println(str.nextToken());
+        }
+        // esempio di token con separatore (punto e virgola)
+        StringTokenizer films = new StringTokenizer("Blade Runner;E.T.;Il Cavaliere Oscuro",";");
+        while(films.hasMoreTokens()){
+            System.out.println(films.nextToken());
+        }
+    }
+}
