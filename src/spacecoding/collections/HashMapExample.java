482b99a1111086060b5d78e703fd839278248bdc
Carlo Contardi <carlocontardi@MacBook-Air-di-Carlo.local>
2024-10-24 10:47:14 +0200

    add HashMap Example

---
+++ src/spacecoding/collections/HashMapExample.java
@@ -0,0 +1,28 @@
+package spacecoding.collections;
+import java.util.HashMap;
+public class HashMapExample{
+    public static void main(String[] args) {
+        HashMap<String, Integer> agesMap = new HashMap<String, Integer>();
+        agesMap.put("Nino",9);
+        agesMap.put("Gino",8);
+        agesMap.put("Alex",2);
+
+        System.out.println("Ages Map : " + agesMap); // non mi piace !
+        System.out.println("Ages Map : " + agesMap.toString());
+
+        // visualizza un età in base ad una chiave
+        System.out.println("Age of Gino: " + agesMap.get("Gino"));
+        // visuallizzo tutte le età con un foreach
+
+        for(String key : agesMap.keySet()){
+            System.out.println("Age of: " +  key + "is" + agesMap.get(key));
+        }
+
+        // rimuove un elemento specificando una chiave
+
+        agesMap.remove("Alex");
+        agesMap.replace("Nino", 8);
+        System.out.println("Ages Map : " + agesMap.toString());
+
+    }
+}
