82cc497a1fce56590fb13d58f1d2f37235f6dbe6
ContardiC <c.contardi@spacecoding.it>
2024-10-28 15:48:55 +0100

    add ThrowException example

---
+++ src/spacecoding/exceptionhandling/ThrowException.java
@@ -0,0 +1,21 @@
+package spacecoding.exceptionhandling;
+
+public class ThrowException {
+    public static double divide(double x, double y ){
+        if( y == 0){
+            throw new ArithmeticException("Divider can not be equal to 0!");
+        }
+        return x / y;
+    }
+    public static void main(String[] args) {
+        double d;
+        try{
+            d = divide(8.5, 2);
+            System.out.println("Result : " + d);
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+     
+        
+    }
+}
