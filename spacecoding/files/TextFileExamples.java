63d5b1860898c3700643bd485a92ffb93d9654ef
ContardiC <c.contardi@spacecoding.it>
2024-12-17 13:13:49 +0100

    updated TextFileExamples.java

--- spacecoding/files/TextFileExamples.java
+++ spacecoding/files/TextFileExamples.java
@@ -1,38 +1,43 @@
 package spacecoding.files;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 class TextFileExamples{
     public static void main(String[] args) {
-        File file = new File("data.txt");
-        char[] array = new char[100];
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader key = new BufferedReader(in);
+        File file = new File("users.txt");
+        String userName= " ";
+        char[] array = new char[1000];
         // scrittura del file
         try{
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write("Un mio gatto si chiama Gino \n");
-            fileWriter.write("e l'altro Nino");
+            FileWriter fileWriter = new FileWriter(file,true);
+            System.out.println("Inserisci il tuo nome: ");
+            userName = key.readLine();
+            fileWriter.write(userName + "\n");
             // dopo aver scritto chiudere lo stream 
             fileWriter.close();
         }catch(IOException e){
             System.out.println("Errore apertura file");
-            e.printStackTrace();
+            // e.printStackTrace(); per il debug
         }
         // lettura del file 
         //FileReader
         try{
-            FileReader reader = new FileReader(file);
+            FileReader fileReader = new FileReader(file);
+            System.out.println("Elenco degli utenti presenti nel file [users.txt]");
             // il metodo read utilizza un array di caratteri
-            reader.read(array);
+            fileReader.read(array);
             System.out.println(array);
-            reader.close();
+            fileReader.close();
         }catch(IOException e){
             System.out.println("Errore apertura file");
             e.printStackTrace();
         }
-
     }
 }
